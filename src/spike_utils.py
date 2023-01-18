1282b162f30d6317341c62ab9d5d92da5de1cf27
Francesco Lässig <flaessig@student.ethz.ch>
2023-01-18 16:50:39 +0100

    continued work on spike train analysis

--- src/spike_utils.py
+++ src/spike_utils.py
@@ -1,5 +1,6 @@
 import cmath
 import numpy as np
+from scipy.signal import argrelextrema
 
 def n_uniform_spikes(n_spikes):
     """
@@ -49,9 +50,8 @@ def spike_fourier(spikes, freq):
 
 def compute_fourier_period_profile(spikes, num_samples):
     """
-    Computes the magnitudes of the fourier values of `spikes`
-    with respect to time periods `i` for `num_samples` equally
-    spaced `i` in [0,1).
+    Computes the fourier values of `spikes` with respect to time periods
+    `i` for `num_samples` equally spaced `i` in [0,1).
     
     Parameters
     ----------
@@ -63,20 +63,18 @@ def compute_fourier_period_profile(spikes, num_samples):
     Returns
     -------
     np.ndarray
-        The `num_samples` magnitudes of the fourier values for the evaluated
-        time periods.
+        The `num_samples` fourier values for the evaluated frequencies.
     """
-    magnitudes = []
+    fourier_vals = []
     time_periods = np.linspace(0, 1, num_samples)
     for T in time_periods:
-        magnitudes.append(abs(spike_fourier(spikes, 1/T)))
-    return magnitudes
+        fourier_vals.append(spike_fourier(spikes, 1/T))
+    return np.array(fourier_vals), time_periods
 
 def compute_fourier_freq_profile(spikes, num_samples):
     """
-    Computes the magnitudes of the fourier values of `spikes`
-    with respect to frequencies `1/i` for `num_samples` equally
-    spaced `i` in [0,1).
+    Computes the fourier values of `spikes` with respect to frequencies
+    `1/i` for `num_samples` equally spaced `i` in [0,1).
     
     Parameters
     ----------
@@ -88,14 +86,13 @@ def compute_fourier_freq_profile(spikes, num_samples):
     Returns
     -------
     np.ndarray
-        The `num_samples` magnitudes of the fourier values for the evaluated
-        frequencies.
+        The `num_samples` fourier values for the evaluated frequencies.
     """
-    magnitudes = []
-    frequencies = 1 / np.linspace(0, 1, num_samples)
+    fourier_vals = []
+    frequencies = (1 / np.linspace(1 / num_samples, 1, num_samples))[::-1]
     for f in frequencies:
-        magnitudes.append(abs(spike_fourier(spikes, f)))
-    return magnitudes
+        fourier_vals.append(spike_fourier(spikes, f))
+    return np.array(fourier_vals), frequencies
 
 def compute_spike_signal(spikes, num_samples):
     """
@@ -123,7 +120,7 @@ def compute_spike_signal(spikes, num_samples):
     return spike_signal
 
 
-def compute_isi_wave(isi, spikes, num_samples):
+def compute_wave(freq, spikes, num_samples):
     """
     Computes a cosine wave with the frequency component
     equal to the inverse of the inter-spike interval `isi`.
@@ -131,9 +128,9 @@ def compute_isi_wave(isi, spikes, num_samples):
 
     Parameters
     ----------
-    isi
-        Float value representing the inter-spike interval used
-        to determine the frequency of the cosine wave to be plotted.
+    freq
+        Float value representing the frequency of the cosine
+        wave to be plotted.
     spikes
         Sorted np.ndarray of timestamps of spikes.
     num_samples
@@ -142,11 +139,56 @@ def compute_isi_wave(isi, spikes, num_samples):
     Returns
     -------
     np.ndarray
-        The cosine signal of the fourier component determined by `isi`
+        The cosine signal of the fourier component determined by `freq`
     """
     time = np.linspace(0, 1, num_samples)
-    freq = 1 / isi
     fourier_value = spike_fourier(spikes, freq)
     phase = cmath.phase(fourier_value)
     wave_signal = np.cos(time * 2 * np.pi * freq + phase)
     return wave_signal
+
+
+def peak_freq_components(spikes, frequency_magnitudes, frequencies, limited_indices=None, eps=0.001):
+    """
+    Computes peaks in the fourier transform of `spikes` that are within `eps`
+    of their maximal value, which corresponds to `len(spikes)`.
+
+    Parameters
+    ----------
+    spikes
+        np.ndarray spike train as sorted time stamps.
+    frquency_magnitudes
+        np.ndarray of the magnitudes of the fourier values
+        of the spikes for each frequency.
+    frequencies
+        np.ndarray containing the chosen frequencies.
+    limit_indices
+        Optionally, a list of integers specifying a subset
+        of indices of peaks to return
+
+        
+    Returns
+    -------
+    Tuple(np.ndarray, np.ndarray)
+        The values and indices of frequencies leading to peak magnitudes
+        of the fourier values of `spikes`
+    """
+    
+    # compute threshold for returned peaks
+    threshold = len(spikes) - eps
+
+    # set all magnitudes that do not correspond to a local maximum to zero
+    non_peak_mask = np.ones(len(frequency_magnitudes))
+    non_peak_mask[argrelextrema(frequency_magnitudes, np.greater)[0]] = 0
+    frequency_magnitudes_filtered = frequency_magnitudes.copy()
+    frequency_magnitudes_filtered[non_peak_mask.astype(bool)] = 0
+
+    # filter frequencies based on epsilon-threshold
+    selected_indices = np.where(frequency_magnitudes_filtered > threshold)[0]
+    selected_frequencies = frequencies[selected_indices]
+
+    if limited_indices is not None:
+        selected_indices = selected_indices[limited_indices]
+        selected_frequencies = selected_frequencies[limited_indices]
+
+    return selected_frequencies, selected_indices
